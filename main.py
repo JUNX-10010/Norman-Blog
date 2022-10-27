120a66b651d6d2309f18b449538a77a917f8ce7f
JUNX-10010 <n.beck10010@gmail.com>
2022-10-27 11:32:38 -0400

    Update Blogpost

--- main.py
+++ main.py
@@ -77,7 +77,7 @@ class BlogPost(db.Model):
     author = relationship("Users", back_populates="post")
     author_api = Column(db.String(250), nullable=True)
     title = db.Column(db.String, unique=True, nullable=False)
-    subtitle = db.Column(db.String, nullable=False)
+    subtitle = db.Column(db.String, nullable=True)
     date = db.Column(db.String(250), nullable=False)
     body = db.Column(db.Text, nullable=False)
     img_url = db.Column(db.String, nullable=False)
@@ -85,7 +85,7 @@ class BlogPost(db.Model):
     user_comment = relationship(Comment, back_populates="parent_post")
 
 
-# db.create_all() #update
+db.create_all() #update
 # Create a function that request new data
 import requests
 
@@ -121,6 +121,8 @@ def data_from_api(data):
 
             if n.author is None:
                 n.author = "unknown"
+            if n.subtitle is None:
+                n.subtitle = " "
             format_data = datetime.strptime(n.date.split(" ")[0], "%Y-%m-%d")
             try:
                 new_post = BlogPost(
